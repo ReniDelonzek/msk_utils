cf4ebe3c7988b865694f0f7c106c77002ee5aa21
reni <renidelonzek@gmail.com>
2020-06-10 15:56:44 -0300

    feat: defaultValue na conversão de data para string

--- lib/extensions/date.dart
+++ lib/extensions/date.dart
@@ -2,12 +2,21 @@ import 'package:intl/intl.dart';
 import 'package:msk_utils/utils/utils_sentry.dart';
 
 extension Date on DateTime {
-  String string(String format) {
+  String string(String format, {String defaultValue}) {
     try {
       return DateFormat(format).format(this);
     } catch (e, stackTrace) {
       UtilsSentry.reportError(e, stackTrace);
-      return '';
+      return defaultValue ?? '';
+    }
+  }
+
+  String stringOrNull(String format, {String defaultValue}) {
+    try {
+      return DateFormat(format).format(this);
+    } catch (e, stackTrace) {
+      UtilsSentry.reportError(e, stackTrace);
+      return defaultValue;
     }
   }
 }
