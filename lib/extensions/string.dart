28c7137c794b886a8622f02de597d046f4bc132f
rdelonzek <renidelonzek@gmail.com>
2020-08-14 21:21:15 -0300

    doc: extensão da classe string

--- lib/extensions/string.dart
+++ lib/extensions/string.dart
@@ -9,11 +9,15 @@ extension St on String {
     return double.tryParse(this.replaceAll(',', '.'));
   }
 
+  /// Retorna um int da string
+  ///
+  /// [defaultValue] Caso o parse falhe, assume o valor indicado
   int toInt({int defaultValue}) {
-    //faz um to double, pq se tentar dar um parse em um decimal direto pra int resulta em erro
+    // Faz um toDouble, pq se tentar dar um parse em um decimal direto pra int resulta em erro
     return this.toDouble(defaultValue: defaultValue?.toDouble() ?? 0.0).toInt();
   }
 
+  /// Adiciona zeros a esquerda de acordo com a [quantidade] indicada
   String addZeros(int quantidade) {
     var string = '';
     for (int i = 0; i < quantidade; i++) {
@@ -26,6 +30,7 @@ extension St on String {
     return this == null || this.isEmpty;
   }
 
+  /// Deixa a primeira letra da palavra em minusculo, e o restantante como estava
   String lowerCaseFirst() {
     if (this != null) {
       if (this.length > 1) {
@@ -36,6 +41,7 @@ extension St on String {
       return this;
   }
 
+  /// Deixa a primeira letra da palavra em maiusculo, e o restantante como estava
   String upperCaseFirst() {
     if (this != null) {
       if (this.length > 1) {
@@ -46,6 +52,7 @@ extension St on String {
       return this;
   }
 
+  /// Deixa a primeira letra da palavra em maiusculo, e o restantante em minúsculo
   String upperCaseFirstLower() {
     if (this != null) {
       if (this.length > 1) {
@@ -57,10 +64,14 @@ extension St on String {
       return this;
   }
 
+  /// Retorna true caso a string seja nula ou vazia
   bool get isNullOrBlank {
     return this == null || this.trim().isEmpty;
   }
 
+  /// Covente string para DateTime
+  ///
+  /// [format] Indica o formato que a data está
   DateTime toDate(String format, {DateTime defaultDate}) {
     try {
       return DateFormat(format).parse(this);
@@ -68,5 +79,13 @@ extension St on String {
       print(e);
       return defaultDate ?? null;
     }
+  }
+
+  /// Retorna a primeira palavra de uma stirng
+  String firstWord() {
+    if (this != null) {
+      return this.split(' ').first;
+    } else
+      return this;
   }
 }
