b92f09cb908ca77f5561df621802eca7ac1b69d4
rdelonzek <renidelonzek@gmail.com>
2020-09-25 16:30:37 -0300

    fix: max value in list extension

--- lib/extensions/list.dart
+++ lib/extensions/list.dart
@@ -1,15 +1,11 @@
-typedef CollumMax<T> = int Function(T obj);
-
-typedef CollumMaxDouble<T> = double Function(T obj);
-
-typedef CollumSumDouble<T> = double Function(T obj);
-
 extension ExList<E> on Iterable<E> {
-  int getMax(CollumMax collumMax, {int defaultValue}) {
-    if (this.isEmpty) return defaultValue;
-    int max = 0;
+  /// Returns max int value in Irarable on [collum] specified
+  /// If Iterable is null or empty, return [defaultValue]
+  int getMax(Function collum, {int defaultValue}) {
+    if (this == null || this.isEmpty) return defaultValue;
+    int max;
     this.forEach((element) {
-      int pMax = collumMax(element);
+      int pMax = collum(element);
       if (pMax != null) {
         if (max == null || pMax > max) {
           max = pMax;
@@ -19,12 +15,15 @@ extension ExList<E> on Iterable<E> {
     return max;
   }
 
-  double getMaxDouble(CollumMaxDouble collumMax) {
-    double max = 0;
+  /// Returns max int value in Irarable on [collum] specified
+  /// If Iterable is null or empty, return null
+  double getMaxDouble(Function collumMax) {
+    if (this.isEmpty) return null;
+    double max;
     this.forEach((element) {
       double pMax = collumMax(element);
       if (pMax != null) {
-        if (pMax > max) {
+        if (max == null || pMax > max) {
           max = pMax;
         }
       }
@@ -32,7 +31,8 @@ extension ExList<E> on Iterable<E> {
     return max;
   }
 
-  double sumByDouble(CollumSumDouble collum) {
+  /// Sum values [collum] specified in Iterable
+  double sumByDouble(Function collum) {
     double sum = 0;
     this?.forEach((element) {
       double c = collum(element);
@@ -43,6 +43,7 @@ extension ExList<E> on Iterable<E> {
     return sum;
   }
 
+  /// Sort list use to Comparable colums
   Iterable<E> sortedBy(Comparable key(E e)) =>
       toList()..sort((a, b) => key(a).compareTo(key(b)));
 }
