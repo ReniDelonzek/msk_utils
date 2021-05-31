863efd53ee663a6ab64e43f207c77db5efe8455b
rdelonzek <renidelonzek@gmail.com>
2021-05-31 10:15:02 -0300

    fix: use maybeOf in Navigator

--- lib/utils/navigation.dart
+++ lib/utils/navigation.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 
 class Navigation {
   static push(BuildContext context, Widget widget, {dynamic args}) async {
-    return await Navigator.of(context).push(new MaterialPageRoute(
+    return await Navigator.maybeOf(context)?.push(new MaterialPageRoute(
         builder: (BuildContext context) => widget,
         settings: RouteSettings(arguments: args)));
   }
 
   static pushReplacement(BuildContext context, Widget widget,
       {dynamic args}) async {
-    return await Navigator.of(context).pushReplacement(new MaterialPageRoute(
-        builder: (BuildContext context) => widget,
-        settings: RouteSettings(arguments: args)));
+    return await Navigator.maybeOf(context)?.pushReplacement(
+        new MaterialPageRoute(
+            builder: (BuildContext context) => widget,
+            settings: RouteSettings(arguments: args)));
   }
 }
